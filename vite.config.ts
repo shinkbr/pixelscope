cdc2851dde788a6d693249d9b4ec85937e47e73d
Shintaro Kobori <1741788+shinkbr@users.noreply.github.com>
2026-02-07 20:40:07 +0900

    Add deploy GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+type MaybeProcess = { process?: { env?: Record<string, string | undefined> } };
+
+const repoName = (globalThis as MaybeProcess).process?.env?.GITHUB_REPOSITORY?.split("/")[1];
+const base = repoName ? `/${repoName}/` : "/";
+
 export default defineConfig({
+  base,
   plugins: [react()],
 });
